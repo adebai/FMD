59a8f0c5e4bbd5e549154c020534bfd3aa812585
Adebowale Ayodeji <theadebai@gmail.com>
2022-08-26 14:41:40 +0100

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,19 +1,10 @@
 let handler = async (m, { conn }) => {
 	
-	// Link Script jgn di ganti ya pliss
-    // Kalo mau masukin github kamu tinggal add aja link githubnya tapi jangan di hapus sumber script!
-    // Jangan Ngeyell..
-    // Kalo ngeyel gk bakal gw up sc lgi.
+	
     
     let txt = `
-Bot ini menggunakan script github
 
-https://github.com/FokusDotId/Family-MD.git
-
-pliss follow dan kasih ✨⭐⭐🌟 ya pak!
-
-My Github: https://github.com/FokusDotId
-
+Thief..
 `
      conn.reply(m.chat, txt, m)
 }
