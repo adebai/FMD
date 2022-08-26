399eb7805f815339b76e73c2c2056668c2056f62
Adebowale Ayodeji <theadebai@gmail.com>
2022-08-26 14:53:34 +0100

    Update anonymous_sendKontak.js

--- plugins/anonymous_sendKontak.js
+++ plugins/anonymous_sendKontak.js
@@ -4,18 +4,18 @@ async function handler(m, { command, usedPrefix, text }) {
 	this.anonymous = this.anonymous ? this.anonymous : {}
 	let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
 	let room = Object.values(this.anonymous).find(room => room.check(who))
-	if (!room) await this.sendButton(m.chat, 'kamu tidak berada di anonymous chat!', 'Mulai Anonymous', usedPrefix + 'start', m)
+	if (!room) await this.sendButton(m.chat, 'you are not in anonymous chat!', 'Start Anonymous', usedPrefix + 'start', m)
 	let other = room.other(who)
 	var name
 	if (text) name = text
 	else name = this.getName(m.sender)
 	var number = who.split('@')[0]
-	if (other) this.reply(other, 'Partner mengirimkan kontak kepadamu', m)
+	if (other) this.reply(other, 'Partner sent you a contact', m)
 	if (other) this.sendContact(other, number, name, m)
 }
-handler.help = ['sendkontak']
+handler.help = ['sendcontact']
 handler.tags = 'anonymous'
-handler.command = /^(sendkontak|kirimkontak)$/i
+handler.command = /^(sendcontact|scnct)$/i
 handler.private = true
 handler.fail = null
 
